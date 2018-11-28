e274bba107fe537ed7bac3e8588b30aa34c27837
Grant Ammons <gammons@gmail.com>
2018-11-28 06:01:32 -0500

    finally get storybook hmr reloading working

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,14 @@
+// follow https://github.com/webpack-contrib/webpack-hot-middleware/issues/129#issuecomment-302310001
+// echo 100000 | sudo tee /proc/sys/fs/inotify/max_user_watches
+// otherwise hot reloading will not work!
+
 import React from 'react'
 import { configure } from '@storybook/react';
 
-const req = require.context('../src/stories', true, /stories\.js$/)
+const req = require.context('../src/', true, /.js$/)
 
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  req.keys().forEach(req)
 }
 
 configure(loadStories, module);
