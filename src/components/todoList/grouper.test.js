6645a2e217b4f8eb7c29d6312e85a7b8f7db3c95
Grant Ammons <gammons@gmail.com>
2018-12-23 06:56:50 -0500

    refactor grouper to be a single function

--- src/components/todoList/grouper.test.js
+++ src/components/todoList/grouper.test.js
@@ -1,5 +1,5 @@
 // @flow
-import grouper from "./grouper"
+import group from "./grouper"
 
 import TodoItemModel from "../../models/todoItem"
 import TodoListGroup from "../../models/todoListGroup"
@@ -8,7 +8,7 @@ import { ALL, BY_CONTEXT, BY_PROJECT } from "../../constants"
 import { todos } from "../../test/test_helper"
 
 it("groups by context", () => {
-  const grouped = grouper.grouped(todos, BY_CONTEXT)
+  const grouped = group(todos, BY_CONTEXT)
   expect(grouped.length).toEqual(6)
 
   const nickGroup = grouped.find(g => g.name === "Nick")
@@ -16,10 +16,9 @@ it("groups by context", () => {
 })
 
 it("groups by project", () => {
-  const grouped = grouper.grouped(todos, BY_PROJECT)
+  const grouped = group(todos, BY_PROJECT)
   expect(grouped.length).toEqual(5)
 
   const projects = ["bigProject","budget","mobile","No projects","testProject"]
   expect(grouped.map(g => g.name)).toEqual(projects)
 })
-
