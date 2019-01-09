d67bffc44d0a9df0d8bc820985b21772f2de78f3
Grant Ammons <gammons@gmail.com>
2019-01-09 06:04:38 -0500

    add backend URLs to constants

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,10 @@
-// *******************
 // Groupings
-// *******************
 export const BY_ALL = "all"
 export const BY_CONTEXT = "context"
 export const BY_PROJECT = "project"
+
+// URLs
+export const PROD_BACKEND_URL = "https://api.ultralist.io"
+export const LOCAL_BACKEND_URL = "http://localhost:3000"
+export const BACKEND_URL =
+  process.env.NODE_ENV === "production" ? PROD_BACKEND_URL : LOCAL_BACKEND_URL
