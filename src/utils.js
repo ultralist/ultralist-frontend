85fe5a18f187fca0e204944f8dd1f695f9d81706
Grant Ammons <gammons@gmail.com>
2019-01-18 15:16:37 -0500

    add getUrlParam

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,19 @@
 const utils = {
   generateUuid: () => {
     return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
-      var r = (Math.random() * 16) | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
+      var r = (Math.random() * 16) | 0,
+        v = c == "x" ? r : (r & 0x3) | 0x8
       return v.toString(16)
     })
+  },
+  getUrlParam: name => {
+    return (
+      decodeURIComponent(
+        (new RegExp("[?|&]" + name + "=" + "([^&;]+?)(&|#|;|$)").exec(
+          window.location.search
+        ) || [null, ""])[1].replace(/\+/g, "%20")
+      ) || null
+    )
   }
 }
 
