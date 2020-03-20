598cc6fe1a073f860673f34da483d856eb994d21
Grant Ammons <gammons@gmail.com>
2020-03-20 06:00:19 -0400

    use correct staging url

--- src/backend/backend.js
+++ src/backend/backend.js
@@ -93,7 +93,7 @@ export const backendUrl = () => {
   if (window.location.hostname === "app.ultralist.io") {
     return "https://api.ultralist.io"
   }
-  if (window.location.hostname === "stag.ultralist.io") {
+  if (window.location.hostname === "staging.ultralist.io") {
     return "https://api-stag.ultralist.io"
   }
   return "http://localhost:3000"
