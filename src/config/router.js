3279598cf471387f34c537de1be5c77ec5ec241c
Grant Ammons <gammons@gmail.com>
2020-05-16 13:07:40 -0400

    fix history deprecation warning

--- src/config/router.js
+++ src/config/router.js
@@ -2,7 +2,7 @@
 
 import React from "react"
 import { Router, Route, Redirect } from "react-router-dom"
-import createBrowserHistory from "history/createBrowserHistory"
+import { createBrowserHistory } from "history"
 
 import TodoList from "../pages/todoList"
 import Login from "../pages/login"
