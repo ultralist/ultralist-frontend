e03f9c044a404e28a343be2fabf47346d7aa109e
Grant Ammons <gammons@gmail.com>
2020-06-13 06:51:57 -0400

    add a script to build a native app

---
+++ build-native-app.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+nativefier --name "Ultralist" "app.ultralist.io" --internal-urls ".*google\.com.*|ultralist\.io.*" -u "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/73.0"
