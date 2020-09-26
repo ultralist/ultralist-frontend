438015be4a0f6d72ff4faa3af21b851ebc0b4c23
Grant Ammons <gammons@gmail.com>
2020-09-26 06:18:31 -0400

    detect view changes and enable/disable saving views

--- src/components/todoList/view.js
+++ src/components/todoList/view.js
@@ -2,22 +2,45 @@
 import React from "react"
 
 import {
+  Badge,
   Button,
   Card,
   CardContent,
   CardActions,
   Container,
+  Tooltip,
   Typography
 } from "@material-ui/core"
 
 import FilterContext from "../utils/filterContext"
+import UserContext from "../utils/userContext"
 
 import FilterChips from "./filterChips"
 import FilterDialog from "./filterDialog"
 
+import FilterModel from "../../shared/models/filter"
+
+const UnsavedChangesSaveButton = () => (
+  <Tooltip title="Save how the list is currently filtered.  You have unsaved changes to your filter.">
+    <Button size="small">
+      <Badge color="secondary" variant="dot">
+        Save
+      </Badge>
+    </Button>
+  </Tooltip>
+)
+const DisabledSaveButton = () => (
+  <Button disabled size="small">
+    Save
+  </Button>
+)
+
 const View = () => {
   const { filter, setFilter } = React.useContext(FilterContext)
+  const { user } = React.useContext(UserContext)
   const [showFilterDialog, setShowFilterDialog] = React.useState(false)
+  const userFilter = new FilterModel(user.views.find(v => v.id === filter.id))
+  const filterChanged = !userFilter.equals(filter)
 
   const onCloseFilterDialog = () => {
     setShowFilterDialog(false)
@@ -27,14 +50,23 @@ const View = () => {
     <Container maxWidth="md">
       <Card variant="outlined">
         <CardContent>
-          <Typography color="textSecondary" gutterBottom>
-            Current view: {filter.name}
-          </Typography>
+          <div>
+            <Typography color="textSecondary" gutterBottom>
+              Current view: {filter.name}
+            </Typography>
+          </div>
+
           <FilterChips onOpenFilterDialog={() => setShowFilterDialog(true)} />
         </CardContent>
         <CardActions>
-          <Button size="small">Load</Button>
-          <Button size="small">Save</Button>
+          <Tooltip title="Load a saved view">
+            <Button size="small">Load</Button>
+          </Tooltip>
+          {filterChanged ? (
+            <UnsavedChangesSaveButton />
+          ) : (
+            <DisabledSaveButton />
+          )}
         </CardActions>
       </Card>
 
